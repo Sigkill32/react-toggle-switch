a9b36b6e15bfe9eff2515fafd01bb9254130fafd
Manoj Bhat <bhatanm@gmail.com>
2020-01-18 21:14:49 +0530

    updated readme

--- example/src/App.js
+++ example/src/App.js
@@ -1,42 +1,101 @@
 import React, { Component } from "react";
 import { ToggleButton } from "react-toggle-button";
+import "./index.css";
 
 export default class App extends Component {
   state = {
-    toggleState: false
+    isCatAlive: 1,
+    doesLikePizza: true,
+    doesLikeReact: true
   };
 
-  handleChange = toggleState => {
-    // this.setState({ toggleState });
-    // console.log(toggleState)
+  handleCat = isCatAlive => {
+    this.setState({ isCatAlive });
+  };
+
+  getCatStat = isCatAlive => {
+    if (isCatAlive === 1) return { state: "YES", color: "green" };
+    if (isCatAlive === 2) return { state: "NO", color: "red" };
+    if (isCatAlive === 3) return { state: "DEAD and ALIVE", color: "blue" };
+  };
+
+  handlePizza = doesLikePizza => {
+    this.setState({ doesLikePizza });
+  };
+
+  handleReact = doesLikeReact => {
+    this.setState({ doesLikeReact });
   };
 
   render() {
-    // const { toggleState } = this.state;
+    const { isCatAlive, doesLikePizza, doesLikeReact } = this.state;
     return (
-      <div style={{margin: '20px'}}>
-        <ToggleButton
-          onChange={this.handleChange}
-          initState={true}
-          buttonDesign='rounded'
-          buttonStates={3}
-          fontSize='12px'
-        />
+      <div style={{ margin: "20px" }}>
+        <div className='toggle-container'>
+          <p>
+            Is Schrodinger's cat alive ? &nbsp; &nbsp;
+            <span
+              style={{
+                color: this.getCatStat(isCatAlive).color,
+                fontSize: "20px"
+              }}
+            >
+              {this.getCatStat(isCatAlive).state}
+            </span>
+          </p>
+          <ToggleButton
+            onChange={this.handleCat}
+            initState={true}
+            buttonDesign='angled'
+            buttonStates={3}
+            fontSize='12px'
+            classNames={{ container: "container", slider: "slider" }}
+            textData={{ stateOne: "YES", stateTwo: "NO", stateThree: "BOTH" }}
+            togglerWidth={40}
+          />
+        </div>
         <br></br>
-        <ToggleButton
-          onChange={this.handleChange}
-          initState={true}
-          buttonDesign='custom'
-          textData={{ stateOne: 'YES', stateTwo: 'NO' }}
-          buttonRadius={{container: '3px', slider: '12px'}}
-        />
+        <div className='toggle-container'>
+          <p>
+            Do you like pizza ? &nbsp; &nbsp;
+            <span
+              style={{
+                fontSize: "20px",
+                color: doesLikePizza ? "green" : "red"
+              }}
+            >
+              {doesLikePizza ? "YES" : "NO"}
+            </span>
+          </p>
+          <ToggleButton
+            onChange={this.handlePizza}
+            initState={true}
+            buttonDesign='rounded'
+            textData={{ stateOne: "YES", stateTwo: "NO" }}
+            classNames={{ container: "container", slider: "slider" }}
+          />
+        </div>
         <br></br>
-        <ToggleButton
-          onChange={this.handleChange}
-          initState={true}
-          buttonDesign='angled'
-          textData={{ stateOne: 'YES', stateTwo: 'NO' }}
-        />
+        <div className='toggle-container'>
+          <p>
+            Do you like React ? &nbsp; &nbsp;
+            <span
+              style={{
+                color: doesLikeReact ? "green" : "red",
+                fontSize: "20px"
+              }}
+            >
+              {doesLikeReact ? "YES" : "NO"}
+            </span>
+          </p>
+          <ToggleButton
+            onChange={this.handleReact}
+            buttonDesign='angled'
+            textData={{ stateOne: "YES", stateTwo: "NO" }}
+            classNames={{ container: "container", slider: "slider" }}
+            initState={false}
+          />
+        </div>
       </div>
     );
   }
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 [![NPM](https://img.shields.io/npm/v/react-toggle-switch.svg)](https://www.npmjs.com/package/react-toggle-switch) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
+<img src="./toggler-demo.gif"></img>
+
 ## Install
 
 ```bash
