892914dadba09b52d0a5108ea8a0f4aa7df10c5e
Manoj Bhat <bhatanm@gmail.com>
2020-01-21 13:15:51 +0530

    changed example

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import ToggleButton from "react-multistate-toggler";
-import "./index.css";
+import React, { Component } from 'react';
+import ToggleButton from 'react-multistate-toggler';
+import './index.css';
 
 export default class App extends Component {
   state = {
@@ -14,9 +14,12 @@ export default class App extends Component {
   };
 
   getCatStat = isCatAlive => {
-    if (isCatAlive === 1) return { state: "YES", color: "green" };
-    if (isCatAlive === 2) return { state: "NO", color: "red" };
-    if (isCatAlive === 3) return { state: "DEAD and ALIVE", color: "blue" };
+    const states = [
+      { state: 'YES', color: 'green' },
+      { state: 'NO', color: 'red' },
+      { state: 'DEAD and ALIVE', color: 'blue' }
+    ];
+    return states[isCatAlive - 1];
   };
 
   handlePizza = doesLikePizza => {
@@ -30,14 +33,14 @@ export default class App extends Component {
   render() {
     const { isCatAlive, doesLikePizza, doesLikeReact } = this.state;
     return (
-      <div style={{ margin: "20px" }}>
-        <div className='toggle-container'>
+      <div style={{ margin: '20px' }}>
+        <div className="toggle-container">
           <p>
             Is Schrodinger's cat alive ? &nbsp; &nbsp;
             <span
               style={{
                 color: this.getCatStat(isCatAlive).color,
-                fontSize: "20px"
+                fontSize: '20px'
               }}
             >
               {this.getCatStat(isCatAlive).state}
@@ -46,53 +49,53 @@ export default class App extends Component {
           <ToggleButton
             onChange={this.handleCat}
             initState={1}
-            buttonDesign='angled'
+            buttonDesign="angled"
             buttonStates={3}
-            fontSize='12px'
-            classNames={{ container: "container", slider: "slider" }}
-            textData={{ stateOne: "YES", stateTwo: "NO", stateThree: "BOTH" }}
+            fontSize="12px"
+            classNames={{ container: 'container', slider: 'slider' }}
+            textData={{ stateOne: 'YES', stateTwo: 'NO', stateThree: 'BOTH' }}
             togglerWidth={40}
           />
         </div>
         <br></br>
-        <div className='toggle-container'>
+        <div className="toggle-container">
           <p>
             Do you like pizza ? &nbsp; &nbsp;
             <span
               style={{
-                fontSize: "20px",
-                color: doesLikePizza ? "green" : "red"
+                fontSize: '20px',
+                color: doesLikePizza ? 'green' : 'red'
               }}
             >
-              {doesLikePizza ? "YES" : "NO"}
+              {doesLikePizza ? 'YES' : 'NO'}
             </span>
           </p>
           <ToggleButton
             onChange={this.handlePizza}
             initState={true}
-            buttonDesign='rounded'
-            textData={{ stateOne: "YES", stateTwo: "NO" }}
-            classNames={{ container: "container", slider: "slider" }}
+            buttonDesign="rounded"
+            textData={{ stateOne: 'YES', stateTwo: 'NO' }}
+            classNames={{ container: 'container', slider: 'slider' }}
           />
         </div>
         <br></br>
-        <div className='toggle-container'>
+        <div className="toggle-container">
           <p>
             Do you like React ? &nbsp; &nbsp;
             <span
               style={{
-                color: doesLikeReact ? "green" : "red",
-                fontSize: "20px"
+                color: doesLikeReact ? 'green' : 'red',
+                fontSize: '20px'
               }}
             >
-              {doesLikeReact ? "YES" : "NO"}
+              {doesLikeReact ? 'YES' : 'NO'}
             </span>
           </p>
           <ToggleButton
             onChange={this.handleReact}
-            buttonDesign='angled'
-            textData={{ stateOne: "YES", stateTwo: "NO" }}
-            classNames={{ container: "container", slider: "slider" }}
+            buttonDesign="angled"
+            textData={{ stateOne: 'YES', stateTwo: 'NO' }}
+            classNames={{ container: 'container', slider: 'slider' }}
             initState={false}
           />
         </div>
